7911b4ab8da95ff22595e4c57a12ae8226bdb1f1
Kevp70 <kevin.petronelli@gmail.com>
2020-10-19 15:18:23 +0200

    add bootstrap style

--- form.php
+++ form.php
@@ -3,13 +3,17 @@ var_dump($_POST);
 if(isset($_POST['submit'])){
 $lastname = $_POST['lastname'];
 $firstname = $_POST['firstname'];
+$email = $_POST['email'];
+$adress = $_POST['adress'];
+$city = $_POST['city'];
+$country = $_POST['country'];
 }
 
 $pdo = new PDO('mysql:host=mysql;dbname=basedetest;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
-$sql = "INSERT INTO user (lastname, firstname) VALUES ('$lastname', '$firstname')";
+$sql = "INSERT INTO user (lastname, firstname, email, adress, city, country) VALUES ('$lastname', '$firstname', '$email', '$adress', '$city', '$country')";
 
 $pdo->exec($sql);
 
--- index.php
+++ index.php
@@ -10,11 +10,48 @@
 </head>
 
 <body>
-    <form action="form.php" method="post">
-        <input type="text" name="lastname" placeholder="lastname">
-        <input type="text" name="firstname" placeholder="firstname">
-        <input type="submit" name="submit">
+    
+    <form class="container pt-5 bg-danger mt-5 rounded-pill text-light" action="form.php" method="post">
+    <h1 class="text-center mb-5">Formulaire</h1>
+        <div class="d-flex pb-5 pl-5 ml-5">
+            <div class="col-md-4">
+                <label for="lastname">Nom</label> <br>
+                <input class="rounded" type="text" name="lastname" placeholder="lastname">
+            </div>
+            <div class="col-md-4">
+                <label for="firstname">Prénom</label> <br>
+                <input class="rounded" type="text" name="firstname" placeholder="firstname">
+            </div>
+            <div class="col-md-4">
+                <label for="email">Email</label> <br>
+                <input class="rounded" type="text" name="email" placeholder="email">
+            </div>
+        </div>
+
+       
+        <div class="col-lg-10 offset-1 pb-5">
+            <label for="adress">Adresse</label> <br>
+            <input class="w-100 rounded" type="text" name="adress" placeholder="adress">
+        </div>
+
+        <div class="d-flex justify-content-around pb-5 pl-5">
+            <div class="col-md-3">
+                <label for="city">Ville</label> <br>
+                <input class="rounded" type="text" name="city" placeholder="city">
+            </div>
+            <div class="col-md-3">
+                <label for="country">Pays</label> <br>
+                <input class="rounded" type="text" name="country" placeholder="country">
+            </div>
+        </div>
+
+        <div class="text-center mt-5 pb-5">
+            <input class="btn-lg btn-outline-danger rounded-pill" type="submit" name="submit">
+        </div>
     </form>
+
+    
+
 </body>
 
 </html>
